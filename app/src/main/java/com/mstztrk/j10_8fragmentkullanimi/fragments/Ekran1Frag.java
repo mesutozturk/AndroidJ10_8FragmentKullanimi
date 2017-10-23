36026c6f305a8d1ca4cb35330e82c2bb25591ed6
Mesut OZTURK <mesutoztrk@msn.com>
2017-10-23 16:23:27 +0300

    fragments

---
+++ app/src/main/java/com/mstztrk/j10_8fragmentkullanimi/fragments/Ekran1Frag.java
@@ -0,0 +1,11 @@
+package com.mstztrk.j10_8fragmentkullanimi.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Mesut on 23.10.2017.
+ */
+
+public class Ekran1Frag extends Fragment {
+
+}
---
+++ app/src/main/java/com/mstztrk/j10_8fragmentkullanimi/fragments/Ekran2Frag.java
@@ -0,0 +1,12 @@
+package com.mstztrk.j10_8fragmentkullanimi.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Mesut on 23.10.2017.
+ */
+
+public class Ekran2Frag extends Fragment {
+
+
+}
